ace7d38c40b4af630d5ee1666ac36ca42894143c
vintovka211-oss <vintovka211@gmail.com>
2026-03-26 16:28:27 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,7 @@
 TOKEN = "8593186262:AAGN6sTyBa1RlJ0eVWwNVzgYUb6aVy_H9LA"
-ADMIN_ID = 8493522297  # Ваш ID
-
-# Курсы валют
-STARS_TO_PAK = 10  # 10 звезд = 100 PAK
-PAK_TO_RUB = 100   # 100 PAK = 1 рубль
-
-# Настройки
-MSG_REWARD = 5  # Награда за сообщение
-MSG_COOLDOWN = 20  # Секунд между наградами
-CLAN_CREATE_COST = 1000  # Стоимость создания клана
-CLAN_REWARD = 50  # Награда за 2 часа в клане
-CLAN_REWARD_INTERVAL = 7200  # 2 часа в секундах
+ADMIN_ID = 8493522297
+MSG_REWARD = 5
+MSG_COOLDOWN = 20
+CLAN_CREATE_COST = 1000
+CLAN_REWARD = 50
+CLAN_REWARD_INTERVAL = 7200
