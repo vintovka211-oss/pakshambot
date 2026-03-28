ed08df0007d1a0ebb190b16b75a7d2c9695b7f6b
vintovka211-oss <vintovka211@gmail.com>
2026-03-28 17:56:46 +0300

    Update config.py

--- config.py
+++ config.py
@@ -4,12 +4,10 @@ BOT_TOKEN = "8590452175:AAGKpZiKBmneyxUX8Ac9U7w9cRjtWQYT8uU"
 ADMIN_IDS = [8493522297]
 
 COIN_NAME = "PAC"
-CURRENCY = "PAC"
-
 PAC_PRICE = 80
 MIN_WITHDRAW_PAC = 250
 PREMIUM_PRICE_PAC = 350
-GAME_COMMISSION = 10  # ← 10% комиссии (ваша прибыль)
+GAME_COMMISSION = 10
 
 PAYMENT_METHODS = {
     "card": "Карта РФ",
@@ -18,13 +16,11 @@ PAYMENT_METHODS = {
 
 YOUR_WALLETS = {
     "card": "2200 1536 7463 9795",
-    "sbp": "+7 927 668 5512"
+    "sbp": "+7 927 668 55 12"
 }
 
 MINE_LEVELS = {
     1: {"name": "⛏️ Каменная", "daily_output": 10, "upgrade_cost": 500, "icon": "🪨"},
     2: {"name": "⚒️ Угольная", "daily_output": 20, "upgrade_cost": 1500, "icon": "⚫"},
     3: {"name": "💎 Железная", "daily_output": 35, "upgrade_cost": 4000, "icon": "🔩"},
-    4: {"name": "✨ Золотая", "daily_output": 60, "upgrade_cost": 10000, "icon": "⭐"},
-    5: {"name": "💎 Алмазная", "daily_output": 100, "upgrade_cost": None, "icon": "💎"},
 }
